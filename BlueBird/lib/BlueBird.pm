0610b1e00b34b7cf5f85360aa57a6c39bb8655e6
ridingrails <stancompedu@yahoo.com>
2015-06-27 02:35:47 +0000

    common follows with errors

--- BlueBird/lib/BlueBird.pm
+++ BlueBird/lib/BlueBird.pm
@@ -10,8 +10,6 @@ our $VERSION = '0.1';
 
 my $twitter = NetTwitter::return_object();
 
-
-
 post '/api/recent' => sub {
     my $data_json = request->body;
     my $data = from_json( $data_json );
@@ -21,10 +19,7 @@ post '/api/recent' => sub {
         try 
         {
             my $user = $data->{'user'};
-	    debug $user;
             my @tweets = $twitter->user_timeline({screen_name => $user});
-            #my $json_tweets = encode_json(\@tweets); 
-            #return $json_tweets;
             return \@tweets;
         }
         catch 
@@ -51,40 +46,12 @@ post '/api/recent' => sub {
 post '/api/common_follows' => sub {
     my $data_json = request->body;
     my $data = decode_json($data_json); 
-    my @user_list = @{$data->{'user_list'}};
     if (exists($data->{'user_list'}))
     {
         try 
         { 
-	    #debug "@user_list\n";
-	    my @follow_array = ();
-	    #for each user in list get follows
-	    for my $i (0 .. $#user_list)
-	    {
-	        my $user_name = $user_list[$i];
-	        debug "$user_name\n";
-	        my @user_names = ();        
-	        for ( my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor} ) {
-		    my $r = $twitter->friends_list({ screen_name => $user_name, cursor => $cursor });
-		    debug "$r->{users}\n";
-		    #my @user_names = ();
-		    my @user_array =  @{ $r->{ users } };
-		    foreach(@user_array)
-		    {
-		        debug $_->{screen_name};
-		        push @user_names, $_->{screen_name};
-		    }
-		    #$r = $twitter->friends_list({ screen_name => $user_name, cursor => $cursor });
-	        } 
-	        push @follow_array, [ @user_names ];
-	        debug "@follow_array\n"
-	    }
-
-	    my $compare_obj = List::Compare->new(@follow_array);
-	    my @intersection = $compare_obj->get_intersection;
-	    #my $json_intersection = encode_json(\@intersection); 
-	    #return $json_intersection;
-	    return \@intersection;
+            my @user_list = @{$data->{'user_list'}}; 
+            return NetTwitter::user_list_intersection(@user_list);
         }
         catch 
         {
@@ -99,7 +66,7 @@ post '/api/common_follows' => sub {
                 send_error(join(' ', "Error getting intersection: ",
                   $exception->code, $exception->message, $exception->error)); 
 	    }
-        }
+        };
     }
     else 
     {
--- BlueBird/lib/NetTwitter.pm
+++ BlueBird/lib/NetTwitter.pm
@@ -1,8 +1,8 @@
 use strict;
 package NetTwitter;
 use Net::Twitter;
+use List::Compare;
 use base 'Exporter';
-#our @EXPORT_OK = ('say_token');
 
 sub return_object {
 	our $nt = Net::Twitter->new(
@@ -15,12 +15,35 @@ sub return_object {
 }
 
 sub user_list_intersection {
-	my $twitter = return_object();
-	my @user_list = @_;
-	our @follow_array = ();
+        my $twitter = return_object();
+        print "$twitter";
+	my (@user_list) = @_;
+	our @follow_array = ();   
 
+        #for each user in list get follows
+        for my $i (0 .. $#user_list)
+        {
+		my $user_name = $user_list[$i];
+		my @user_names = ();
+
+		#iterate over cursor returned by twitter endpoint to get all follows
+		for ( my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor} ) {
+		      my $r = $twitter->friends_list({ screen_name => $user_name, cursor => $cursor });
+		      my @user_array =  @{ $r->{ users } };
+		      foreach(@user_array)
+		      {
+			   push @user_names, $_->{screen_name};
+		      } 
+		}
+	        push @follow_array, [ @user_names ]; 
+        }
+
+        my $compare_obj = List::Compare->new(@follow_array);
+        our @intersection = $compare_obj->get_intersection;
+        return \@intersection;
 }
 
 1;
+
 # good until invalidated, with ...
 #$nt->invalidate_token
